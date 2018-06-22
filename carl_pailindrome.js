ba36b0bdc32ea172f26c2885b2868c52928caefa
Pengxuan <pengxuan@u.nus.edu>
2018-06-22 10:49:56 +0800

    add array testing

--- carl_pailindrome.js
+++ carl_pailindrome.js
@@ -1,14 +1,16 @@
-let strOri = 'aab AA';
-let strPail = true ;
-
-strCheck = strOri.toLowerCase();
-strCheck = strCheck.replace(/\s/g, '');
-
-let strLen = strCheck.length;
-for (let n = 0; n < Math.floor(strLen/2); n++){
-	if (strCheck[n] !== strCheck[strLen-n-1]) {
-		strPail = false;
-		break;
+let strOri = ['Aabb AA', 'aaBaA', 'aadfds'];
+strOri.forEach ( (strOri) => {
+	
+	strCheck = strOri.toLowerCase();
+	strCheck = strCheck.replace(/\s/g, '');
+	
+	let strLen = strCheck.length;
+	let strPail = true ;
+	for (let n = 0; n < Math.floor(strLen/2); n++){
+		if (strCheck[n] !== strCheck[strLen-n-1]) {
+			strPail = false;
+			break;
+		}
 	}
-}
-console.log (`The pailindrome check for string "${strOri}" is: ${strPail}`);
+	console.log (`The pailindrome check for string "${strOri}" is: ${strPail}`);
+})
